dd009238a80c46a55d3c7767f16f1e555140b70f
nagitak <stom1028@naver.com>
2022-06-19 21:50:36 +0900

    resolve rendering error

--- front/src/components/Article.tsx
+++ front/src/components/Article.tsx
@@ -2,24 +2,18 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Atricles = () => {
-  const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState(new Array());
   useEffect(() => {
     axios
       .get('http://localhost:1000/api/articles/show')
       .then(function (response) {
-        console.log('response === ', response);
         setArticles(response?.data.main);
-        console.log('댕댕한바퀴~');
       })
       .catch(function (err) {
-        console.log(err.response.data);
-        alert(err.response.data);
-        console.log('댕댕한바퀴~ 에러~');
+        alert(err?.response.data);
       });
   }, []);
-
-  console.log(articles ? `잘됨 ${JSON.stringify(articles)}` : '안됨');
-  return <div>{articles ? articles : '안됨'}</div>;
+  return <div>{articles.map((v) => `${v.title}\n`)}</div>;
 };
 
 export default Atricles;
